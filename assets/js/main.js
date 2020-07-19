d23aa70b90b7e37c7ea81b8d1a241163a6dc5ee5
GGG <ggg@MacBook-Pro-de-GGG.local>
2020-07-19 11:11:52 +0200

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("document chargé");
 
   const contactForm = document.querySelector("#contactForm");
+  const button = document.querySelector("button");
+
   contactForm.addEventListener("submit", async (event) => {
     // console.log("CLICK");
     event.preventDefault();
+    button.setAttribute("disabled", "disabled");
+    button.classList.add("btn-disabled");
     const data = {
       firstname: document.querySelector("#firstname").value,
       lastname: document.querySelector("#lastname").value,
@@ -13,7 +17,20 @@ document.addEventListener("DOMContentLoaded", () => {
       message: document.querySelector("#message").value,
     };
     // console.log(data);
-    const response = await axios.post("http://localhost:3000/", data);
+    const response = await axios.post(
+      "https://formulaire-backend-greg.herokuapp.com/",
+      data
+    );
     console.log(response);
+    if (response.status !== 200) {
+      alert("Le formulaire n'a pas pu etre envoyé !");
+      button.removeAttribute("disabled");
+      button.classList.remove("btn-disabled");
+    } else {
+      alert("Le formulaire a bien ete envoyé !");
+      button.removeAttribute("disabled");
+      button.classList.remove("btn-disabled");
+      contactForm.reset();
+    }
   });
 });
